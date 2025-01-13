8045d13ed523ef666384e27d8f1b74b1821b3e19
JoseBlue17 <josemiguelramirezc0@gmail.com>
2025-01-13 16:31:01 -0500

    New files

--- Java/src/main/java/com/demo/demo/Demo1Application.java
+++ Java/src/main/java/com/demo/demo/Demo1Application.java
@@ -3,11 +3,7 @@ package com.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication(scanBasePackages = {"com.demo","com.demo.controller", "com.demo.services", "com.demo.config"})
@@ -18,17 +14,5 @@ public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
-    @Configuration
-    public static class Myconfiguration{
-        @Bean
-        public WebMvcConfigurer corsConfigurer(){
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-                }
-            };
-        }
-    }
+
 }
